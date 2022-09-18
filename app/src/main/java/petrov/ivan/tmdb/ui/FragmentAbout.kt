52c9621c3a6cf15480806f7959c272ed21e02949
Иван <i.petrov@fitmost.ru>
2022-09-18 11:46:26 +0400

    add Chrome Custom Tab

--- app/src/main/java/petrov/ivan/tmdb/ui/FragmentAbout.kt
+++ app/src/main/java/petrov/ivan/tmdb/ui/FragmentAbout.kt
@@ -1,18 +1,70 @@
 package petrov.ivan.tmdb.ui
 
+import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.browser.customtabs.CustomTabColorSchemeParams
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import petrov.ivan.tmdb.R
+import petrov.ivan.tmdb.databinding.FragmentAboutBinding
+import petrov.ivan.tmdb.ui.custom_tab.CustomTabActivityHelper
 
 class FragmentAbout : Fragment() {
+
+    private lateinit var binding: FragmentAboutBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val spanTxt = SpannableStringBuilder(getString(R.string.fragment_about_information))
+        val url = getString(R.string.api_url)
+        spanTxt.append(url)
+        spanTxt.setSpan(
+            object : ClickableSpan() {
+                override fun onClick(widget: View) {
+                    val customTabsIntent = CustomTabsIntent.Builder()
+                        .setDefaultColorSchemeParams(
+                            CustomTabColorSchemeParams
+                                .Builder()
+                                .setToolbarColor(resources.getColor(R.color.primaryColor))
+                                .build()
+                        ).build()
+                    CustomTabActivityHelper.openCustomTab(
+                        requireActivity(),
+                        customTabsIntent,
+                        Uri.parse(url),
+                        fallback = null
+                    )
+                }
+                override fun updateDrawState(ds: TextPaint) {
+                    ds.linkColor = ContextCompat.getColor(requireContext(), R.color.colorAccent)
+                    super.updateDrawState(ds)
+                }
+            },
+            spanTxt.length - url.length,
+            spanTxt.length,
+            0
+        )
+
+        binding.textVeiw.movementMethod = LinkMovementMethod.getInstance()
+        binding.textVeiw.setText(spanTxt, TextView.BufferType.SPANNABLE)
     }
 }
